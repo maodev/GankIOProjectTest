6ba10a5e884817849e8382e3ff5635aa02f6db28
maodev <maoyudev@gmail.com>
2016-12-09 16:39:12 +0800

    Initial commit

--- app/src/main/java/me/maodev/gankioproject/LoadRecyclerOnScrollListener.java
+++ app/src/main/java/me/maodev/gankioproject/LoadRecyclerOnScrollListener.java
@@ -30,18 +30,12 @@ public abstract class LoadRecyclerOnScrollListener extends RecyclerView.OnScroll
             if (totalItemCount > previousTotal) {
                 loading = false;
                 previousTotal = totalItemCount;
-<<<<<<< HEAD
             }else{
                 previousTotal = 0;
             }
         }
         if (!loading&& (totalItemCount - visibleItemCount) <= firstVisibleItem) {
-=======
-            }
-        }
-        if (!loading
-                && (totalItemCount - visibleItemCount) <= firstVisibleItem) {
->>>>>>> origin/master
+
             currentPage++;
             onLoadMore();
             loading = true;
